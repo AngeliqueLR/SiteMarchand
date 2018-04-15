2b85964616414269f95e412ae8344364c5c6ecfd
AngeliqueLR <angelique.le-roux@gmx.fr>
2018-04-15 14:03:58 +0200

    15/04/18 14:03

--- application/controllers/Administrateur.php
+++ application/controllers/Administrateur.php
@@ -8,6 +8,30 @@
 
             $this->load->library('session');
             if ($this->session->statut==0)
+            {
+                $this->load->helper('url');
+                redirect('/Visiteur/seConnecter');
+            }
+        }
+
+        public function ajouterUnProduit()
+        {
+            $this->load->helper('form');
+            $this->load->library('form_validation');
+
+            $DonneesEnvoyees['TitreDeLaPage'] = 'Ajouter un article';
+            
+            //regles de validations
+            $this->form_validation->set_rules('txtNomProduit', 'Nom du produit', 'required');
+            $this->form_validation->set_rules('txtPrixProduit', 'Prix HT du produit', 'required');
+            $this->form_validation->set_rules('txtTVAProduit', 'Taux de TVA du produit', 'required');
+            $this->form_validation->set_rules('txtPhotoProduit', 'Première photo du produit', 'required');
+            $this->form_validation->set_rules('txtPhotoBisProduit', 'Deuxième photo du produit', 'required');
+            $this->form_validation->set_rules('tbxQuantiteProduit', 'Quantité de produit disponible', 'required');
+            
+            
+            
+            
         }
     }
 ?>
