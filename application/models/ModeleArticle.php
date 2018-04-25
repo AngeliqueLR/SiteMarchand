90367413620d5798d6def080e29e393c13ec1970
AngeliqueLR <angelique.le-roux@gmx.fr>
2018-04-25 10:12:12 +0200

    25/04/18 10:12

--- application/models/ModeleArticle.php
+++ application/models/ModeleArticle.php
@@ -10,13 +10,13 @@
         {
             if($pNoProduit === FALSE)
             {
-                $this->db->select('NOPRODUIT, LIBELLE, PRIXHT, TAUXTVA, NOMIMAGE, NOMIMAGEBIS');
+                $this->db->select('NOPRODUIT, LIBELLE, PRIXHT, TAUXTVA, NOMIMAGE, NOMIMAGEBIS, Promotion');
                 $requete = $this->db->get('produit');
 
                 return $requete->result_array();
             }
 
-            $this->db->select('LIBELLE, PRIXHT, TAUXTVA, QUANTITEENSTOCK, NOMIMAGE, NOMIMAGEBIS, DETAIL');
+            $this->db->select('LIBELLE, PRIXHT, TAUXTVA, QUANTITEENSTOCK, NOMIMAGE, NOMIMAGEBIS, DETAIL, Promotion');
             $requete = $this->db->get_where('produit', array('NOPRODUIT' => $pNoProduit));
 
             return $requete->row_array();
@@ -66,7 +66,23 @@
         public function retournerArticlesParDates($pDate)
         {
             $this->db->select('NOPRODUIT, LIBELLE, PRIXHT, TAUXTVA, NOMIMAGE, NOMIMAGEBIS');
-            $requete = $this->db->get_where('produit', array('DA' => $pMarqueChoisie));
+            $requete = $this->db->get_where('produit', array('NOPRODUIT IN (SELECT NOPRODUIT FROM produit WHERE DATEDIFF( NOW( ) , DATEAJOUT ) <'.$pDate.')'));
+
+            return $requete->row_array();
+        }
+
+        public function retournerCategories()
+        {
+            $this->db->select('distinct(categorie.LIBELLE)');
+            $requete = $this->db->get_where('produit, categorie', array('categorie.NOCATEGORIE = produit.NOCATEGORIE'));
+
+            return $requete->row_array();
+        }
+
+        public function retournerMarques()
+        {
+            $this->db->select('distinct(NOM)');
+            $requete = $this->db->get_where('produit, marque', array('marque.NOMARQUE = produit.NOMARQUE'));
 
             return $requete->row_array();
         }
--- application/views/Visiteur/CatalogueAvecPagination.php
+++ application/views/Visiteur/CatalogueAvecPagination.php
@@ -8,7 +8,7 @@
             foreach ($LesProduits as $unProduit):
                 echo '<div class="col-sm-4">';
                 $prix = $unProduit['PRIXHT'] + ($unProduit['PRIXHT'] * $unProduit['TAUXTVA'] / 100);
-                echo '<p>'.anchor('Visiteur/VoirUnProduit/'.$unProduit['NOPRODUIT'], $unProduit['LIBELLE']).'</p><p>'.anchor('Visiteur/VoirUnProduit/'.$unProduit['NOPRODUIT'], $prix.' €').'</p><p>'.anchor('Visiteur/VoirUnProduit/'.$unProduit['NOPRODUIT'], $unProduit['QUANTITEENSTOCK']).'</p><p>'.anchor('Visiteur/VoirUnProduit/'.$unProduit['NOPRODUIT'], img_onmouseover($unProduit['NOMIMAGE'], $unProduit['NOMIMAGEBIS'])).'</p>';
+                echo '<p>'.anchor('Visiteur/VoirUnProduit/'.$unProduit['NOPRODUIT'], $unProduit['LIBELLE']).'</p><p>'.anchor('Visiteur/VoirUnProduit/'.$unProduit['NOPRODUIT'], $prix.' €').'</p><p><span class="glyphicon glyphicon-shopping-cart"></span></p><p>'.anchor('Visiteur/VoirUnProduit/'.$unProduit['NOPRODUIT'], img_onmouseover($unProduit['NOMIMAGE'], $unProduit['NOMIMAGEBIS'])).'</p>';
                 echo '</div>';
                 $nbProduits = $nbProduits + 1;
                 if ($nbProduits%3 == 0)
